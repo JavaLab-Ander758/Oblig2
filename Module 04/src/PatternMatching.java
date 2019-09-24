1d5949b467036d6c90cd3008717fec8fc022a88d
ander758 <anders.rubach.ese@hotmail.com>
2019-09-24 14:55:05 +0200

    Added method in PatternMatching

--- Module 04/src/PatternMatching.java
+++ Module 04/src/PatternMatching.java
@@ -1,22 +1,32 @@
-import java.util.*;
- /*
+/*
 Skriv et program som får inn to strenger fra bruker,  str og pattern,  og som sjekker ut om pattern finnes inni str,
 dersom den finnes så skal program returnere indeks for første tegn av pattern inni str, ellers skal den returnere -1.
 */
-public class PatternMatching {
-     public String matching(String str, String pattern) {
+class PatternMatching {
 
-         int index = -1;
-         int k = 0;
-         for (int i = 0; i < str.length(); i++) {
-             if (k == pattern.length()) {
-                 //returner index av start match
-             } else
-                 System.out.print(str.charAt(i));
-                 k++;
-   // else
-          //   k = 0;
-         }
-         return "Lol";
-     }
- }
+    /**
+     * Finds a pattern in a given string
+     *
+     * @param str The string to search in
+     * @param pattern The pattern to search for in str
+     * @return The index of first char of pattern, if not found: -1
+     */
+    private int findPatternInString(String str, String pattern) {
+        int index = -1;
+        for (int i = 0, k = 0; i < str.length(); i++) {
+            System.out.printf("%s:%s i=%s\n", k, str.charAt(i), i);
+            if (str.charAt(i) == pattern.charAt(k)) {
+                k++;
+                if (k == pattern.length()) {
+                    index = i - k + 1;
+                    break;
+                }
+            } else {
+                k = 0;
+                if (str.charAt(i) == pattern.charAt(k))
+                    k++;
+            }
+        }
+        return index;
+    }
+}
