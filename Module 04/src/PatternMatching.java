4dbd091922b9ba0131c4666bd9357d4f65bb917b
ajo239 <ajo239@post.uit.no>
2019-09-24 13:50:23 +0200

    Start something with Pattern

--- Module 04/src/PatternMatching.java
+++ Module 04/src/PatternMatching.java
@@ -4,17 +4,19 @@ Skriv et program som får inn to strenger fra bruker,  str og pattern,  og som s
 dersom den finnes så skal program returnere indeks for første tegn av pattern inni str, ellers skal den returnere -1.
 */
 public class PatternMatching {
-    public static void main(String[] args) {
-        PatternMatching patternMatching = new PatternMatching();
-        String str = ""; // Text to be searched
-        String pattern = ""; // Pattern to search for
-        Scanner input = new Scanner(System.in);
+     public String matching(String str, String pattern) {
 
-        System.out.println("- Skriv inn til programmet -\n");
-        System.out.print("str: ");
-        str = input.nextLine();
-        pattern = input.nextLine();
-
-        
-    }
-}
+         int index = -1;
+         int k = 0;
+         for (int i = 0; i < str.length(); i++) {
+             if (k == pattern.length()) {
+                 //returner index av start match
+             } else
+                 System.out.print(str.charAt(i));
+                 k++;
+   // else
+          //   k = 0;
+         }
+         return "Lol";
+     }
+ }
--- Module 04/test/PatternMatchingTest.java
+++ Module 04/test/PatternMatchingTest.java
@@ -1,5 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PatternMatchingTest {
+    PatternMatching patternMatching = new PatternMatching();
+
+    @Test
+    public void test_something(){
+
+        assertThat(patternMatching.matching("Dette er en test", "test"), is("Lol"));
+    }
 
 }
